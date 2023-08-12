371b71dd5b133b7734851eb9ffd977474fda39d0
Luiz Gustavo <112131771+luizguga@users.noreply.github.com>
2023-08-12 17:41:49 -0300

    animação do pato

--- scripts/script.js
+++ scripts/script.js
@@ -5,6 +5,43 @@ for(let i=0; i<coracoes.length; i++){
     coracoes[i].addEventListener('click', ()=>{janela()});
 }
 
-function janela(){
+const patos = document.querySelectorAll('img.pato');
+
+let controle = true;
+
+const janela = () => {
     telaEscondida.style.display = 'flex';
+    loop();
 }
+
+const loop = () => {
+    this.loop = setInterval(()=>{
+        switch(controle){
+            case true:
+                patos[0].classList.add('animacao-p1');
+                setTimeout(()=>{
+                    patos[0].classList.remove('animacao-p1');
+                    controle = false;
+                },3000);
+            break;
+            case false:
+                patos[1].classList.add('animacao-p2');
+                setTimeout(()=>{
+                    patos[1].classList.remove('animacao-p2');
+                    controle = true;
+                },3000);
+            break;
+        }
+    },500);
+}
+
+function fecharJanela() {
+    telaEscondida.style.display = 'none';
+    clearInterval(this.loop);
+    patos[0].classList = 'pato pato-a';
+    patos[1].classList = 'pato pato-b';
+    controle = true;
+}
+
+patos[0].addEventListener('click', ()=>{fecharJanela()});
+patos[1].addEventListener('click', ()=>{fecharJanela()});
